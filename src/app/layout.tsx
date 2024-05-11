04cbe46f1d05b267c09975b84b55d0fe7291a464
zak <zakariaelougui@gmail.com>
2024-05-11 20:22:05 +0200

    backup commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from 'next';
+import { DM_Sans } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/providers/me-provider';
+
+const font = DM_Sans({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Fuzzie',
+  description: 'Automate Your Work With Fuzzie',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,10 @@
+'use client';
+import React from 'react';
+
+type Props = {};
+
+const MenuOptions = (props: Props) => {
+  return <div>MenuOptions</div>;
+};
+
+export default MenuOptions;
